3c6bea6edf1bf1bc2a51a4c8873abdd26839a0de
Jeff West <jeff10278@me.com>
2024-06-07 15:02:08 -0500

    V0.1.0 create docs (#9)
    * ready for readthedocs
    
    * ready for readthedocs

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Py Benzinga API"
+copyright = "2024, TexasCoding"
+author = "TexasCoding"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.duration",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "autoapi.extension",
+    "nbsphinx",
+]
+autoapi_type = "python"
+autoapi_dirs = [f"{Path(__file__).parents[2]}/src"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
